8177d8ff178796fc4b53049aa6f3b051f55539a3
AmitDavidi <65807063+AmitDavidi@users.noreply.github.com>
2021-09-10 14:06:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,206 @@
+import pygame
+from ctypes import *
+import numpy as np
+import os
+import math
+
+
+cwd = os.getcwd()
+path = os.path.join(cwd + '/myf.so')
+
+# get C Functions
+c_funcs = CDLL(path)
+c_funcs.map_num.argtypes = [c_double, c_double, c_double, c_double]
+c_funcs.does_converge.argtypes = [c_double, c_double, c_int, c_int, c_int]
+c_funcs.does_converge.restype = c_double
+c_funcs.map_num.restype = c_double
+
+pygame.init()
+consts = {
+    "WIDTH": 640,
+    'HEIGHT': 640,
+    'MAX_ITERATIONS': 200}
+
+""""
+replaced by C- function
+
+def is_converge(x, y, iterations):
+    i = 0
+    # first gen is x + iy
+    z_n_imaginary = y
+    z_n_real = x
+
+    while i < iterations:
+        # imaginary of next gen = old gen squared + y - imaginary part
+        new_z_n_imaginary = 2 * z_n_real * z_n_imaginary + y
+
+        # real of next gen = old gen squared + x - real part
+        new_z_n_real = z_n_real * z_n_real - z_n_imaginary * z_n_imaginary + x
+
+        z_n_real = new_z_n_real
+        z_n_imaginary = new_z_n_imaginary
+        if (z_n_real * z_n_real + z_n_imaginary * 2) > 4:
+            # diverges
+            color = map2(i, 0, iterations, 10, 250)
+            # return color based on iterations
+            return color, color, color
+
+        i += 1
+    # converge
+    return 0, 0, 0
+map2(double value, double min, double max, double wanted_min, double wanted_max)
+"""
+
+
+# def c_funcs_map_num(value, Max, wanted_min, wanted_max):
+#    x = c_funcs.map_num(value, Max, wanted_min, wanted_max)
+#    return x
+
+
+def draw_screen(win, screen_matrix):
+    pygame.pixelcopy.array_to_surface(win, screen_matrix)
+
+
+def main(constants, win, data):
+    colors = [(0, 0, 0),
+              (66, 30, 15),
+              (25, 7, 26),
+              (9, 1, 47),
+              (4, 4, 73),
+              (0, 7, 100),
+              (12, 44, 138),
+              (24, 82, 177),
+              (57, 125, 209),
+              (134, 181, 229),
+              (211, 236, 248),
+              (241, 233, 191),
+              (248, 201, 95),
+              (255, 170, 0),
+              (204, 128, 0),
+              (153, 87, 0),
+              (106, 52, 3)]
+
+    # better names for constants
+    width = constants['WIDTH']
+    height = constants['HEIGHT']
+    max_iterations = constants['MAX_ITERATIONS']
+
+    # starting ranges for the Mandelbrot
+    x_range = [-2, 2]
+    y_range = [-2, 2]
+    last_x_range = []
+    last_y_range = []
+
+    # display calculations
+    screen_matrix = np.zeros((height, width, 3), dtype=np.uint8)
+    flag = 1
+    # zoom variables
+    zoom_counter = 1
+    diff = 30
+    diff_width = 2 * diff
+
+    # Coloring the set
+    max_color = 250
+    min_color = 0
+
+    running = True
+    while running:
+        mx, my = pygame.mouse.get_pos()
+        # draw rectangle around mouse
+
+        pygame.pixelcopy.array_to_surface(win, screen_matrix)
+        current_rect = pygame.Rect(mx - diff, my - diff, diff_width, diff_width)
+        pygame.draw.rect(win, (230, 230, 230), current_rect, 1)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    running = False
+                elif event.key == pygame.K_SPACE:
+                    x_range = [-2, 2]
+                    y_range = [-2, 2]
+                    flag = 1
+                    zoom_counter = 1
+                    max_iterations = consts['MAX_ITERATIONS']
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed(3)[0]:
+                    mapped_x1 = c_funcs.map_num(mx - diff, width, x_range[0], x_range[1])
+                    mapped_x2 = c_funcs.map_num(mx + diff, width, x_range[0], x_range[1])
+                    mapped_y1 = c_funcs.map_num(my - diff, height, y_range[0], y_range[1])
+                    mapped_y2 = c_funcs.map_num(my + diff, height, y_range[0], y_range[1])
+                    last_x_range.append(x_range)
+                    last_y_range.append(y_range)
+                    zoom_counter += 1
+                    x_range = [mapped_x1, mapped_x2]
+                    y_range = [mapped_y1, mapped_y2]
+                    flag = 1
+
+                if pygame.mouse.get_pressed(3)[2]:
+                    if zoom_counter != 1:
+                        x_range = last_x_range.pop(-1)
+                        y_range = last_y_range.pop(-1)
+                        zoom_counter = max(zoom_counter - 1, 1)
+                        flag = 1
+                max_iterations = zoom_counter * consts['MAX_ITERATIONS']
+
+        if flag:  # draw the picture
+
+            x = 0
+            while x < width:
+                mapped_x = c_funcs.map_num(x, width, x_range[0], x_range[1])
+                y = 0
+                while y < height:
+                    mapped_y = c_funcs.map_num(y, height, y_range[0], y_range[1])
+
+                    try:
+                        Smoothed_iters = data[(mapped_x, mapped_y)]
+
+                    except KeyError:
+                        Smoothed_iters = c_funcs.does_converge(mapped_x, mapped_y, max_iterations, min_color, max_color)
+                        data[(mapped_x, mapped_y)] = Smoothed_iters
+
+                    # Color_mod_17 - the index of the color in the color array
+                    Color_mod_17 = Smoothed_iters % 17
+
+                    # how close and how far to the next color - 0 to 1
+                    how_close = Color_mod_17 % 1
+                    how_far = 1 - how_close
+
+                    # most efficient way to round Smoothed_iters to an integer
+                    Smoothed_iters = math.floor(Color_mod_17)
+                    # most efficient - currently known to me to round Smoothed_iters + 1 to be in range
+                    ColorP1_mod_17 = (Smoothed_iters + 1) % 17
+
+                    Color = (how_far * colors[Smoothed_iters][0] + how_close * colors[ColorP1_mod_17][0],
+                             how_far * colors[Smoothed_iters][1] + how_close * colors[ColorP1_mod_17][1],
+                             how_far * colors[Smoothed_iters][2] + how_close * colors[ColorP1_mod_17][2])
+                    screen_matrix[x, y] = Color
+
+                    y += 1
+                x += 1
+
+            flag = 0
+    return data
+
+
+datadict = {}
+# with open('C:\\Users\\amitd\\source\\repos\\Project21\\Project21\\data.txt','r') as data:
+# with open('data.txt', 'r') as data:
+#    for line in data.readlines():
+#        x, y, val = line.strip().replace('\x00', '').split(',')
+#        valD = int(val)
+#        xD = double(x)
+#        yD = double(y)
+#        datadict[(xD, yD)] = valD
+#
+# print(datadict)
+
+
+WIN = pygame.display.set_mode((consts['WIDTH'], consts['HEIGHT']))
+pygame.display.set_caption('The Mandelbrot set - A Glimpse Into Infinity')
+main(consts, WIN, datadict)
---
+++ myf.c
@@ -0,0 +1,46 @@
+#include <math.h>
+
+double map_num(double value, double max, double wanted_min, double wanted_max) {
+	return ( (value/max) *(wanted_max - wanted_min)) + wanted_min;
+}
+
+double log_2(double x){
+	return log(x)/log(2);
+}
+
+double does_converge(double x_0, double y_0, int iterations, int min_color, int max_color){
+	double i = 0; // iter
+
+
+	int color = 0;	
+	double x = 0;
+	double y = 0;
+			
+	double x_2 = 0;
+	double y_2 = 0;
+	double w = 0;
+	
+	while((i < iterations) && ((x_2 + y_2) <= 16)){
+		
+
+		x = x_2 - y_2 + x_0;
+		y= w - x_2 - y_2 +y_0;
+		x_2 = x*x;
+		y_2 = y*y;
+		w = (x+y)*(x+y);
+
+		i++;
+
+	}
+	if(i==iterations)
+		return 0;
+	else{
+	double smoothed = log_2(log_2(x_2 + y_2) / 2 );
+	
+	i = (double)(sqrt(i + 1 - smoothed)) ;
+	
+	return i;
+}
+
+	}
+
